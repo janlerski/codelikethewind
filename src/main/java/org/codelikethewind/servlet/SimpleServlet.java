1e59b4dceb82350c2503aa1869aea4897b993386
Jan Lerski <jal@miracle.dk>
2022-10-07 11:01:13 +0200

    Updated SimpleServlet

--- src/main/java/org/codelikethewind/servlet/SimpleServlet.java
+++ src/main/java/org/codelikethewind/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "Hello Miracle" + "</h1>");
+		out.println("<h1>" + "Hello World Miracle" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
