c5783ce11e26ebe755e858f38ba0f85f17818487
Jan Lerski <jal@miracle.dk>
2022-10-21 11:49:42 +0200

    running on udv

--- src/main/java/org/codelikethewind/servlet/SimpleServlet.java
+++ src/main/java/org/codelikethewind/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "Hello World, it's Monday !!!!" + "</h1>");
+		out.println("<h1>" + "Hello World, it's Friday !!" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
